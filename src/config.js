78139ae56866af0f6f51482b81cdc32308d4ccc8
Zachary Davidson <zachary.j.davidson@gmail.com>
2024-03-29 16:13:14 -0400

    adds scraper functionality

---
+++ src/config.js
@@ -0,0 +1,14 @@
+//config for scraper search parameters
+
+export default
+{
+
+    "queries": [
+        'boat', 'ship', 'sailboat', 'submarine',
+        'modern_boat', 'modern_ship', 'modern_sailboat', 'modern_submarine',
+        'old_boat', 'old_ship', 'old_sailboat', 'old_submarine', 'ocean'
+    ],
+
+    "quantity": "25",
+
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,52 @@
 import puppeteer from 'puppeteer';
+import * as config from './config.js';
+
+
+const results = {};
+const quantity = config.default.quantity;
+const queries = config.default.queries;
 
 (async () => {
-
-  // Launch the browser
   const browser = await puppeteer.launch();
-
-  // Create a page
   const page = await browser.newPage();
 
-  // Go to your site
-  await page.goto('https://google.com');
+  for (let index = 0; index < queries.length; index++){
+    const query = queries[index];
+    results[query] = [];
+    console.log(`Searching for query: ${query}`);
+    await page.goto('https://images.google.com');
 
-  // Query for an element handle.
-  const imgElement = await page.waitForSelector('img');
+    //Type query with gLFyf google class name selector
+    await page.type('.gLFyf', query);
+    await page.keyboard.press('Enter');
 
-  console.log(imgElement)
+    //wait for results
+    await page.waitForNavigation();
 
-  await page.screenshot({
-    path: 'first-screenshot.png'
-  });
+    //click first image to pull up side window to expose full sized image
+    await page.waitForSelector('#islrg > div.islrc > div:nth-child(2) > a.FRuiCf.islib.nfEiy > div.fR600b.islir');
+    
+    await page.click('#islrg > div.islrc > div:nth-child(2) > a.FRuiCf.islib.nfEiy > div.fR600b.islir');
 
-  // Dispose of handle
-  await imgElement.dispose();
+    for (let j = 0; j < quantity; j++) {
+      try {
+        //wait for image to render
+        await page.waitForSelector('#Sva75c > div.A8mJGd.NDuZHe.OGftbe-N7Eqid-H9tDt > div.LrPjRb > div.AQyBn > div.tvh9oe.BIB1wf > c-wiz > div > div > div > div > div.v6bUne > div.p7sI2.PUxBg > a > img.sFlh5c.pT0Scc.iPVvYb');
+        
+        //collect image src
+        const imgSrc = await page.$eval('#Sva75c > div.A8mJGd.NDuZHe.OGftbe-N7Eqid-H9tDt > div.LrPjRb > div.AQyBn > div.tvh9oe.BIB1wf > c-wiz > div > div > div > div > div.v6bUne > div.p7sI2.PUxBg > a > img.sFlh5c.pT0Scc.iPVvYb', img => img.src);
+        results[query].push(imgSrc)
+        //click arrow to go to next image
+        await page.click('#Sva75c > div.A8mJGd.NDuZHe.OGftbe-N7Eqid-H9tDt > div.LrPjRb > div.AQyBn > div.tvh9oe.BIB1wf > c-wiz > div > div > div > div > div.s9n5ef.VTMWGb > div > div.HJRshd > button:nth-child(2) > div')
+        
+      } catch({ name, message }) {
+        console.log(`${name} error at index: ${j}: ${message}`);
+        //save page error screenshot
+        await page.screenshot({path: `images/errors/error${j}.jpeg`});
 
-  // Close browser.
+        }
+    }
+  }
+  //await page.screenshot({path: `scraped-images/screencap.jpeg`});
   await browser.close();
-
 })();
