1d94e6ce49352c0122b58ac525a5e63d87f3376e
Zachary Davidson <zachary.j.davidson@gmail.com>
2024-04-15 22:01:28 -0400

    adding web scraping for duck duck go

--- src/index.js
+++ src/index.js
@@ -2,51 +2,46 @@ import puppeteer from 'puppeteer';
 import * as config from './config.js';
 
 
-const results = {};
+const scraped = [];
 const quantity = config.default.quantity;
 const queries = config.default.queries;
+
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  for (let index = 0; index < queries.length; index++){
+  for (let index = 0; index < queries.length; index++) {
     const query = queries[index];
-    results[query] = [];
+
     console.log(`Searching for query: ${query}`);
-    await page.goto('https://images.google.com');
-
-    //Type query with gLFyf google class name selector
-    await page.type('.gLFyf', query);
+    await page.goto('https://duckduckgo.com');
+    await page.type('#searchbox_input', query);
     await page.keyboard.press('Enter');
-
-    //wait for results
     await page.waitForNavigation();
 
-    //click first image to pull up side window to expose full sized image
-    await page.waitForSelector('#islrg > div.islrc > div:nth-child(2) > a.FRuiCf.islib.nfEiy > div.fR600b.islir');
-    
-    await page.click('#islrg > div.islrc > div:nth-child(2) > a.FRuiCf.islib.nfEiy > div.fR600b.islir');
+    //click images tab
+    await page.waitForSelector('#duckbar_static > li:nth-child(2) > a');
+    await page.click('#duckbar_static > li:nth-child(2) > a');
 
-    for (let j = 0; j < quantity; j++) {
+    // grab required results images from query
+    const results = [];
+  
+    for (let i = 0; i < 2;i++) {
       try {
-        //wait for image to render
-        await page.waitForSelector('#Sva75c > div.A8mJGd.NDuZHe.OGftbe-N7Eqid-H9tDt > div.LrPjRb > div.AQyBn > div.tvh9oe.BIB1wf > c-wiz > div > div > div > div > div.v6bUne > div.p7sI2.PUxBg > a > img.sFlh5c.pT0Scc.iPVvYb');
-        
-        //collect image src
-        const imgSrc = await page.$eval('#Sva75c > div.A8mJGd.NDuZHe.OGftbe-N7Eqid-H9tDt > div.LrPjRb > div.AQyBn > div.tvh9oe.BIB1wf > c-wiz > div > div > div > div > div.v6bUne > div.p7sI2.PUxBg > a > img.sFlh5c.pT0Scc.iPVvYb', img => img.src);
-        results[query].push(imgSrc)
-        //click arrow to go to next image
-        await page.click('#Sva75c > div.A8mJGd.NDuZHe.OGftbe-N7Eqid-H9tDt > div.LrPjRb > div.AQyBn > div.tvh9oe.BIB1wf > c-wiz > div > div > div > div > div.s9n5ef.VTMWGb > div > div.HJRshd > button:nth-child(2) > div')
-        
-      } catch({ name, message }) {
-        console.log(`${name} error at index: ${j}: ${message}`);
-        //save page error screenshot
-        await page.screenshot({path: `images/errors/error${j}.jpeg`});
+        await page.waitForSelector(`#zci-images > div > div.tile-wrap > div.zci__main.zci__main--tiles.js-tiles.has-nav.tileview__images.has-tiles--grid > div:nth-child(${3+i}) > div.tile--img__media > span > img`);
+        const imgSrc = await page.$eval(`#zci-images > div > div.tile-wrap > div.zci__main.zci__main--tiles.js-tiles.has-nav.tileview__images.has-tiles--grid > div:nth-child(${3+i}) > div.tile--img__media > span > img`, img => img.src);
+        results.push(imgSrc);
 
-        }
+      } catch({name , message}) {
+        console.log(`${name} error at index: ${i}: ${message}`);
+        //save page error screenshot
+        await page.screenshot({path: `images/errors/error${i}.jpeg`});
+      }
     }
+    scraped[query] = results;
   }
-  //await page.screenshot({path: `scraped-images/screencap.jpeg`});
   await browser.close();
 })();
+
+console.log(scraped);
--- src/yolo/yolov8.py
+++ src/yolo/yolov8.py
@@ -1,6 +1,22 @@
 from ultralytics import YOLO
-from paths import image_path
+from paths import image2_path
+import json
+import requests
 
+# f = open('/Users/zacharydavidson/Documents/GitHub/yolo-ship-detection/src/yolo/data.json')
+# data = json.load(f)
+
+# Load a pretrained YOLOv8n model
 model = YOLO('yolov8n.pt')
-source = '/Users/zacharydavidson/Documents/GitHub/yolo-ship-detection/src/yolo/image.png'
-results = model(source, save=True)
+
+# Define remote image or video URL
+source = "https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.5kZSADQEUFFwg7PdyZ5TvwHaFj%26pid%3DApi&f=1&ipt=da802b8193c4104a71bedf56174aa68f6b8d92eddefb3cbf2d4231ebdc848ee7&ipo=images.png"
+source2 = "https://tse1.mm.bing.net/th?id=OIP.5kZSADQEUFFwg7PdyZ5TvwHaFj&pid=Api&f=1&ipt=da802b8193c4104a71bedf56174aa68f6b8d92eddefb3cbf2d4231ebdc848ee7&ipo=images"
+
+
+# Run inference on the source
+results = model(source2, save=True, stream=True)  # list of Results objects
+# f.close()
+
+# <a href="https://duckduckgo.com/?t=h_&amp;q=boat&amp;iax=images&amp;ia=images&amp;iai=http%3A%2F%2F4.bp.blogspot.com%2F-49L1_ik2dEo%2FUbRervcGdnI%2FAAAAAAAALOo%2F3_0Rd-K8Kvg%2Fs1600%2FHdhut.blogspot.com%2B%2525284%252529.jpeg#
+#                                                                                     'https:  /   / 4.bp.blogspot.com/  -49L1_ik2dEo/  UbRervcGdnI/  AAAAAAAALOo/  3_0Rd-K8Kvg/s1600/Hdhut.blogspot.com+%25284%2529.jpeg'
